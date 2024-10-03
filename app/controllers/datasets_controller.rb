1fbb13dde45d7e78f2bb81f530848416d3b92029
Celia King <celia@blackboxco.com.au>
2024-10-03 19:55:12 +1000

    the file upload works, but no schema is generated

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,46 +1,46 @@
 class DatasetsController < ApplicationController
-  def new
-    @dataset = Dataset.new
-  end
-
-  def create
-    @dataset = Dataset.new(dataset_params)
-    if @dataset.save
-      @dataset.file.attach(params[:dataset][:file])
-      # Parse the dataset and generate schema
-      schema_definition = generate_schema(@dataset)
-      Schema.create(dataset: @dataset, schema_definition: schema_definition)
+    def new
+      @dataset = Dataset.new
+    end
+  
+    def create
+      @dataset = Dataset.new(dataset_params)
+      if @dataset.save
+        @dataset.file.attach(params[:dataset][:file])
+        # Parse the dataset and generate schema
+        schema_definition = generate_schema(@dataset)
+        Schema.create(dataset: @dataset, schema_definition: schema_definition)
+        redirect_to @dataset
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @dataset = Dataset.find(params[:id])
+      @schema = @dataset.schema
+    end
+  
+    def validate
+      @dataset = Dataset.find(params[:id])
+      new_dataset = params[:file] # Assuming the new dataset is uploaded as a file
+      if @dataset.schema.validate_dataset(new_dataset)
+        flash[:notice] = "Dataset is valid according to the schema."
+      else
+        flash[:alert] = "Dataset is not valid according to the schema."
+      end
       redirect_to @dataset
-    else
-      render :new
+    end
+  
+    private
+  
+    def dataset_params
+      params.require(:dataset).permit(:name)
+    end
+  
+    def generate_schema(dataset)
+      # Logic to parse dataset and generate schema
+      # This is a placeholder for actual implementation
+      "Generated Schema"
     end
   end
-
-  def show
-    @dataset = Dataset.find(params[:id])
-    @schema = @dataset.schema
-  end
-
-  def validate
-    @dataset = Dataset.find(params[:id])
-    new_dataset = params[:file] # Assuming the new dataset is uploaded as a file
-    if @dataset.schema.validate_dataset(new_dataset)
-      flash[:notice] = "Dataset is valid according to the schema."
-    else
-      flash[:alert] = "Dataset is not valid according to the schema."
-    end
-    redirect_to @dataset
-  end
-
-  private
-
-  def dataset_params
-    params.require(:dataset).permit(:name)
-  end
-
-  def generate_schema(dataset)
-    # Logic to parse dataset and generate schema
-    # This is a placeholder for actual implementation
-    "Generated Schema"
-  end
-end
